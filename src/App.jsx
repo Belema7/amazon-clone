961938672983a3ef1b0e09cfb2f225c8a167f080
Belema Girma <belemagirma31@gmail.com>
2025-11-20 22:07:51 -0800

    add Carousel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 // import Header from './components/Header/Header'
 import HeaderMain from './components/Header/HeaderMain'
+import HeroCarousel from './components/carousel/HeroCarousel'
+
+
 
 const App = () => {
   return (
     <div>
         <HeaderMain/>
+        <HeroCarousel/>
     </div>
   )
 }
---
+++ src/components/carousel/HeroCarousel.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { img } from "./images/data"; // adjust path if needed
+
+const HeroCarousel = () => {
+  return (
+    <div className="relative w-full">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        interval={3500}
+        transitionTime={800}
+        showThumbs={false}
+        showStatus={false}
+        showArrows={true}
+        swipeable={true}
+        emulateTouch={true}
+        stopOnHover={false}
+        dynamicHeight={false}
+        // ⭐ Fade effect (Amazon-style)
+        animationHandler="fade"
+        renderArrowPrev={(onClick, hasPrev, label) =>
+          hasPrev && (
+            <button
+              onClick={onClick}
+              title={label}
+              className="absolute left-4 top-1/2 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white p-3 rounded-full z-10"
+            >
+              ❮
+            </button>
+          )
+        }
+        renderArrowNext={(onClick, hasNext, label) =>
+          hasNext && (
+            <button
+              onClick={onClick}
+              title={label}
+              className="absolute right-4 top-1/2 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white p-3 rounded-full z-10"
+            >
+              ❯
+            </button>
+          )
+        }
+      >
+        {img.map((image, index) => (
+          <div key={index} className="relative">
+            <img
+              src={image}
+              alt={`Slide ${index + 1}`}
+              className="w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] object-cover"
+            />
+          </div>
+        ))}
+      </Carousel>
+
+      {/* ⭐ Amazon bottom fade overlay */}
+      <div className="absolute bottom-0 w-full h-40 bg-gradient-to-t from-[#EAEDED] to-transparent pointer-events-none"></div>
+    </div>
+  );
+};
+
+export default HeroCarousel;
