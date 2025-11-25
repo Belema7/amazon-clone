1299d20633144ede03ccb368f8227daf0e0ae49a
belema7 <belemagirma31@gmail.com>
2025-11-25 12:15:55 -0800

    add routing for category

--- src/pages/Results/Results.jsx
+++ src/pages/Results/Results.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LayOut from '../../components/LayOut/LayOut'
 import { useParams } from 'react-router-dom'
-
+import { productUrl } from '../../Api/endPoints';
+import ProductCard from '../../components/Product/ProductCard';
+import axios from 'axios'
 const Results = () => {
+    const [results, setResults] = useState([]);
     const {categoryName} = useParams();
+
+    useEffect(()=> {
+        axios.get(`${productUrl}/products/category/${categoryName}`)
+    .then((res) => {
+      setResults(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    },[])
     return (
         <LayOut>
-            
+            <section>
+                <h1>Results</h1>
+                <p>Category: {categoryName}</p>
+                <hr />
+                <div>
+                    {results?.map((product)=> (
+                        <ProductCard key={product.id} product={product}/>
+                    ))}
+                </div>
+            </section>
         </LayOut>
     )
 }
