8c6953097c29b2dd19e86e9ddeff202202f639b2
belema7 <belemagirma31@gmail.com>
2025-11-29 08:45:25 -0800

    add login page

---
+++ src/components/Utility/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3ADpw5SnBnv1KnRUhLmcKBoybdS4yzZ4",
+  authDomain: "clone-a6b2b.firebaseapp.com",
+  projectId: "clone-a6b2b",
+  storageBucket: "clone-a6b2b.firebasestorage.app",
+  messagingSenderId: "91514635306",
+  appId: "1:91514635306:web:d2cdf1c76e64eb9446af53"
+};
+
+const app = initializeApp(firebaseConfig);
